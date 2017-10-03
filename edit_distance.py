c9170966ccc7a682479ed8604fcfef8d245df4c1
Whale Steward <onpeci@gmail.com>
2017-10-04 02:22:17 +0700

    edit distance

--- edit_distance.py
+++ edit_distance.py
@@ -8,37 +8,39 @@ def json_open(fl):
     return d
 
 def mktree(node, child,count=0):
-    print (count)    
+    print (count)
     if isinstance(child,list):
         for c in child:
             count+=1
             return mktree(node,c,count)
-    
+
     elif isinstance(child,dict):
         for k,v in child.items():
             if isinstance(child[k], dict):
-                node.addkid(Node(k)) 
+                node.addkid(Node(k))
                 return mktree(node,v, count)
             else :
-                node.addkid(Node(v))                          
+                node.addkid(Node(k))
+    return node
 #(Node("f").addkid(Node("a").addkid(Node("h")).addkid(Node("c").addkid(Node("l")))).addkid(Node("e"))
 
-
-
-def handle_dict(dictionary):
-    for k in dictionary:
-        if (isinstance(k,dict)):
-            handle_dict(k)
-        else :
-            return k
-
+def loop_tree(dictionary, node):
+    print node
+    print "----"
+    for k,v in dictionary.iteritems():
+        if isinstance(dictionary[k], dict):
+            loop_tree(dictionary[k], node.addkid(k))
+        else:
+            node.addkid(Node(v))
+            # print k,v
 
 if __name__ == '__main__':
     node = Node('A')
-    fl = sys.argv[1]    
-    obj = json_open(fl)
+    # fl = sys.argv[1]
+    fl = "test.json"
+    json_obj = json_open(fl)
 
-    print (handle_dict(obj[0]))
-
-    #res = mktree(node,obj)
-    #print(res)
+    loop_tree(json_obj[0], node)
+    print node
+    # res = mktree(node,obj)
+    # print(res)
